f556b0bfe27b30f87d8b6fcc09ad933ddac5b705
SandaruwanWGMA <sandaruwanwgma@gmail.com>
2024-10-15 16:31:39 +0530

    check 13

--- train.py
+++ train.py
@@ -62,6 +62,8 @@ def main():
         print("Checkpoint 5")
 
         for i, data in enumerate(train_loader):
+            print("Checkpoint 6")
+
             high_res_images = data[1]
             low_res_images = data[0]
 
@@ -77,62 +79,62 @@ def main():
 
             current_batch_size = len(data[0])
             total_iters += current_batch_size
-            print("Checkpoint 6")
+            print("Checkpoint 7")
 
             mri_vol = {"LR": low_res_images, "HR": high_res_images}
 
-            model.set_input(mri_vol)  # Prepare input data by slicing the MRI volume
-            print("Checkpoint 7")
+    #         model.set_input(mri_vol)  # Prepare input data by slicing the MRI volume
+    #         print("Checkpoint 7")
 
-            # Process each slice in the current volume
-            num_slices = len(model.lr_slices)
-            for slice_index in range(num_slices):
-                lr_slice, hr_slice = model.get_slice_pair(slice_index)
+    #         # Process each slice in the current volume
+    #         num_slices = len(model.lr_slices)
+    #         for slice_index in range(num_slices):
+    #             lr_slice, hr_slice = model.get_slice_pair(slice_index)
 
-                model.optimize_parameters(
-                    lr_images=lr_slice, hr_images=hr_slice, lambda_tv=1.0
-                )  # Forward, backward pass, and optimize
+    #             model.optimize_parameters(
+    #                 lr_images=lr_slice, hr_images=hr_slice, lambda_tv=1.0
+    #             )  # Forward, backward pass, and optimize
 
-                # Print loss information at the specified frequency
-                if total_iters % opt.print_freq == 0:
-                    losses = model.get_current_losses()
-                    t_comp = (time.time() - epoch_start_time) / epoch_iter
-                    visualizer.print_current_losses(epoch, epoch_iter, losses, t_comp)
+    #             # Print loss information at the specified frequency
+    #             if total_iters % opt.print_freq == 0:
+    #                 losses = model.get_current_losses()
+    #                 t_comp = (time.time() - epoch_start_time) / epoch_iter
+    #                 visualizer.print_current_losses(epoch, epoch_iter, losses, t_comp)
 
-                # Save the latest model at the specified frequency
-                if total_iters % opt.save_latest_freq == 0:
-                    print(
-                        "Saving the latest model (epoch %d, total_iters %d)"
-                        % (epoch, total_iters)
-                    )
-                    save_checkpoint(
-                        model, opt.checkpoint_dir, "latest", epoch, total_iters
-                    )
+    #             # Save the latest model at the specified frequency
+    #             if total_iters % opt.save_latest_freq == 0:
+    #                 print(
+    #                     "Saving the latest model (epoch %d, total_iters %d)"
+    #                     % (epoch, total_iters)
+    #                 )
+    #                 save_checkpoint(
+    #                     model, opt.checkpoint_dir, "latest", epoch, total_iters
+    #                 )
 
-            # Display visuals at the specified frequency of the slices of a certain MRI Volume
-            if total_iters % opt.display_freq == 0:
-                model.save_volume(epoch=epoch)
+    #         # Display visuals at the specified frequency of the slices of a certain MRI Volume
+    #         if total_iters % opt.display_freq == 0:
+    #             model.save_volume(epoch=epoch)
 
-            # Save the model at the end of every epoch
-            if epoch % opt.save_epoch_freq == 0 and i == len(train_loader) - 1:
-                print(
-                    "Saving the model at the end of epoch %d, iters %d"
-                    % (epoch, total_iters)
-                )
-                save_checkpoint(
-                    model, opt.checkpoint_dir, "epoch_%d" % epoch, epoch, total_iters
-                )
+    #         # Save the model at the end of every epoch
+    #         if epoch % opt.save_epoch_freq == 0 and i == len(train_loader) - 1:
+    #             print(
+    #                 "Saving the model at the end of epoch %d, iters %d"
+    #                 % (epoch, total_iters)
+    #             )
+    #             save_checkpoint(
+    #                 model, opt.checkpoint_dir, "epoch_%d" % epoch, epoch, total_iters
+    #             )
 
-            print(
-                "End of epoch %d / %d \t Time Taken: %d sec"
-                % (
-                    epoch,
-                    opt.n_epochs + opt.n_epochs_decay,
-                    time.time() - epoch_start_time,
-                )
-            )
+    #         print(
+    #             "End of epoch %d / %d \t Time Taken: %d sec"
+    #             % (
+    #                 epoch,
+    #                 opt.n_epochs + opt.n_epochs_decay,
+    #                 time.time() - epoch_start_time,
+    #             )
+    #         )
 
-    model.save_final_models()
+    # model.save_final_models()
 
 
 if __name__ == "__main__":
