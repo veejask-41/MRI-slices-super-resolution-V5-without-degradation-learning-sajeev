a38b6e8399f2d3b9c28991ae696b63ce56329797
SandaruwanWGMA <sandaruwanwgma@gmail.com>
2024-10-15 15:16:15 +0530

    commit

--- train.py
+++ train.py
@@ -16,9 +16,9 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def main():
     # Set manual seed for reproducibility
-    torch.manual_seed(999)
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed_all(999)
+    # torch.manual_seed(999)
+    # if torch.cuda.is_available():
+    #     torch.cuda.manual_seed_all(999)
 
     # Parse options
     opt = TrainOptions().parse()
@@ -47,79 +47,79 @@ def main():
     dataset_size = len(train_dataset)
     print(f"The number of training images = {dataset_size}")
 
-    # Create visualizer
-    visualizer = Visualizer(opt)
+    # # Create visualizer
+    # visualizer = Visualizer(opt)
 
-    # Optionally resume training
-    if opt.continue_train:
-        load_checkpoint(model, opt.checkpoint_dir, opt.which_epoch, str(device))
-        print(f"Loading checkpoint on device: {device}")
+    # # Optionally resume training
+    # if opt.continue_train:
+    #     load_checkpoint(model, opt.checkpoint_dir, opt.which_epoch, str(device))
+    #     print(f"Loading checkpoint on device: {device}")
 
-    # Training loop
-    total_iters = 0
-    for epoch in range(opt.epoch_count, opt.n_epochs + opt.n_epochs_decay + 1):
-        epoch_start_time = time.time()
-        epoch_iter = 0
+    # # Training loop
+    # total_iters = 0
+    # for epoch in range(opt.epoch_count, opt.n_epochs + opt.n_epochs_decay + 1):
+    #     epoch_start_time = time.time()
+    #     epoch_iter = 0
 
-        for i, data in enumerate(train_loader):
-            high_res_images = data[1]
-            low_res_images = data[0]
-            current_batch_size = len(data[0])
-            total_iters += current_batch_size
+    #     for i, data in enumerate(train_loader):
+    #         high_res_images = data[1]
+    #         low_res_images = data[0]
+    #         current_batch_size = len(data[0])
+    #         total_iters += current_batch_size
 
-            mri_vol = {"LR": low_res_images, "HR": high_res_images}
+    #         mri_vol = {"LR": low_res_images, "HR": high_res_images}
 
-            model.set_input(mri_vol)  # Prepare input data by slicing the MRI volume
+    #         model.set_input(mri_vol)  # Prepare input data by slicing the MRI volume
 
-            # Process each slice in the current volume
-            num_slices = len(model.lr_slices)
-            for slice_index in range(num_slices):
-                lr_slice, hr_slice = model.get_slice_pair(slice_index)
+    #         # Process each slice in the current volume
+    #         num_slices = len(model.lr_slices)
+    #         for slice_index in range(num_slices):
+    #             lr_slice, hr_slice = model.get_slice_pair(slice_index)
 
-                model.optimize_parameters(
-                    lr_images=lr_slice, hr_images=hr_slice, lambda_tv=1.0
-                )  # Forward, backward pass, and optimize
+    #             model.optimize_parameters(
+    #                 lr_images=lr_slice, hr_images=hr_slice, lambda_tv=1.0
+    #             )  # Forward, backward pass, and optimize
 
-                # Print loss information at the specified frequency
-                if total_iters % opt.print_freq == 0:
-                    losses = model.get_current_losses()
-                    t_comp = (time.time() - epoch_start_time) / epoch_iter
-                    visualizer.print_current_losses(epoch, epoch_iter, losses, t_comp)
+    #             # Print loss information at the specified frequency
+    #             if total_iters % opt.print_freq == 0:
+    #                 losses = model.get_current_losses()
+    #                 t_comp = (time.time() - epoch_start_time) / epoch_iter
+    #                 visualizer.print_current_losses(epoch, epoch_iter, losses, t_comp)
 
-                # Save the latest model at the specified frequency
-                if total_iters % opt.save_latest_freq == 0:
-                    print(
-                        "Saving the latest model (epoch %d, total_iters %d)"
-                        % (epoch, total_iters)
-                    )
-                    save_checkpoint(
-                        model, opt.checkpoint_dir, "latest", epoch, total_iters
-                    )
+    #             # Save the latest model at the specified frequency
+    #             if total_iters % opt.save_latest_freq == 0:
+    #                 print(
+    #                     "Saving the latest model (epoch %d, total_iters %d)"
+    #                     % (epoch, total_iters)
+    #                 )
+    #                 save_checkpoint(
+    #                     model, opt.checkpoint_dir, "latest", epoch, total_iters
+    #                 )
 
-            # Display visuals at the specified frequency of the slices of a certain MRI Volume
-            if total_iters % opt.display_freq == 0:
-                model.save_volume(epoch=epoch)
+    #         # Display visuals at the specified frequency of the slices of a certain MRI Volume
+    #         if total_iters % opt.display_freq == 0:
+    #             model.save_volume(epoch=epoch)
 
-            # Save the model at the end of every epoch
-            if epoch % opt.save_epoch_freq == 0 and i == len(train_loader) - 1:
-                print(
-                    "Saving the model at the end of epoch %d, iters %d"
-                    % (epoch, total_iters)
-                )
-                save_checkpoint(
-                    model, opt.checkpoint_dir, "epoch_%d" % epoch, epoch, total_iters
-                )
+    #         # Save the model at the end of every epoch
+    #         if epoch % opt.save_epoch_freq == 0 and i == len(train_loader) - 1:
+    #             print(
+    #                 "Saving the model at the end of epoch %d, iters %d"
+    #                 % (epoch, total_iters)
+    #             )
+    #             save_checkpoint(
+    #                 model, opt.checkpoint_dir, "epoch_%d" % epoch, epoch, total_iters
+    #             )
 
-            print(
-                "End of epoch %d / %d \t Time Taken: %d sec"
-                % (
-                    epoch,
-                    opt.n_epochs + opt.n_epochs_decay,
-                    time.time() - epoch_start_time,
-                )
-            )
+    #         print(
+    #             "End of epoch %d / %d \t Time Taken: %d sec"
+    #             % (
+    #                 epoch,
+    #                 opt.n_epochs + opt.n_epochs_decay,
+    #                 time.time() - epoch_start_time,
+    #             )
+    #         )
 
-    model.save_final_models()
+    # model.save_final_models()
 
 
 if __name__ == "__main__":
