411b1e2eaa1bd4f23492c9c806e37c539283f6c9
SandaruwanWGMA <sandaruwanwgma@gmail.com>
2024-10-21 15:05:38 +0530

    check 68

--- model/create_model.py
+++ model/create_model.py
@@ -1,4 +1,4 @@
-from .SRUNet import SRUNet
+from .SRUNet import SimpleNet
 from .degradation_network import DegradationNetwork
 from .VGGStylePatchGAN import VGGStylePatchGAN
 from .SuperResolutionModel import SuperResolutionModel
@@ -8,15 +8,18 @@ def create_model(opt):
     if opt.model_type == "super_resolution_model":
         return SuperResolutionModel(opt)
     elif opt.model_type == "sr_unet":
-        return SRUNet(
-            image_size=opt.image_size,
-            in_channels=opt.in_channels,
-            out_channels=opt.out_channels,
-            freeze_encoder=opt.freeze_encoder,
-        )
+        return SimpleNet()
     elif opt.model_type == "multi_gdn":
         return DegradationNetwork(image_size=opt.image_size)
     elif opt.model_type == "vgg_patch_gan":
         return VGGStylePatchGAN(patch_size=70)
     else:
         raise ValueError(f"Unknown model type: {opt.model_type}")
+
+
+# SRUNet(
+#     image_size=opt.image_size,
+#     in_channels=opt.in_channels,
+#     out_channels=opt.out_channels,
+#     freeze_encoder=opt.freeze_encoder,
+# )
