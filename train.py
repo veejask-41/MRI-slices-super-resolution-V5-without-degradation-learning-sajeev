48ed470c1ac7859a83e1c56cff6a6c458a0ebfc9
SandaruwanWGMA <sandaruwanwgma@gmail.com>
2024-10-23 08:05:23 +0530

    training 04

--- train.py
+++ train.py
@@ -23,8 +23,8 @@ def main():
     model = create_model(opt)
 
     # Creating dataset instances
-    train_dataset = MRIDataset("./dataset/train_filenames.txt", limit=10)
-    val_dataset = MRIDataset("./dataset/val_filenames.txt", limit=10)
+    train_dataset = MRIDataset("./dataset/train_filenames.txt")
+    val_dataset = MRIDataset("./dataset/val_filenames.txt")
 
     # Creating data loaders
     train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True)
